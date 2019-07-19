c979be00b41eb2aee4381eea8c47d05192360345
Aida Abdikulova <abdikulova@gmail.com>
2019-07-19 05:50:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,14 +13,14 @@ class CashRegister
 
   def apply_discount()
     new_variable = @discount.to_f/100
-    if @discount > 0 
+    if @discount > 0
       @total = (1-new_variable) * @total
       return "After the discount, the total comes to $#{@total.to_i}."
-  else 
+  else
       return "There is no discount to apply."
 
   end
 
-  
+
 
 end
