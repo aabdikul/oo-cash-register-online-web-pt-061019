cdd92b549f39bcdf49550029555c121d440a5cdf
Aida Abdikulova <abdikulova@gmail.com>
2019-07-19 06:00:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
 
   def add_item(item,price,quantity=1)
     @total += price * quantity
-    @list = quantity.times do {@list << item}
+    quantity.times do {@list << item}
     end
   end
 
